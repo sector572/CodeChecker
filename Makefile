e85534565762075dc6e81baef82a1246fb6a2d19
DLarionov <fairness@mail.ru>
2019-03-20 14:32:40 +0300

    Добавлен пример обнаружения ошибки разъименования нулевого указателя

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: TestExecutable
 
-TestExecutable: main.o test1.o test2.o 
-	g++ main.o test1.o test2.o -o TestExecutable
+TestExecutable: main.o test1.o test2.o test3.o
+	g++ main.o test1.o test2.o test3.o -o TestExecutable
 
 main.o: main.cpp
 	g++ -c main.cpp
@@ -11,6 +11,9 @@ test1.o: test1.cpp
 
 test2.o: test2.cpp
 	g++ -c test2.cpp
+
+test3.o: test3.cpp
+	g++ -c test3.cpp
 
 clean:
 	rm -rf *.o TestExecutable    
--- main.cpp
+++ main.cpp
@@ -2,11 +2,22 @@
 #include <cstdlib>
 #include "test1.h"
 #include "test2.h"
+#include "test3.h"
+
 using namespace std;
 
 
 int main() {
+	std::cout<<"Start program"<<std::endl;
+
+	std::cout<<"Проверка: Ошибка двойного освобождения памяти."<<std::endl;
 	mem_multiple_free(); // Ошибка двойного освобождения памяти
+
+	std::cout<<"Проверка: Ошибка с межпроцедурным вычислением индекса"<<std::endl;
 	test_buffer_index(); // Пример ошибки с межпроцедурным вычислением индекса
+
+	std::cout<<"Проверка: Ошибка разыменование нулевого указателя"<<std::endl;
+	test_null_pointer_dereference();
+
 	return 0;
 }
