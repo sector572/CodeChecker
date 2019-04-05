b0014cc77027245ea13efc8dbad6fc61dc844716
developer1 <e.kuznetsova@avelamedia.ru>
2019-04-06 00:30:25 +0300

    Изменен запуск тестов.

--- main.cpp
+++ main.cpp
@@ -11,31 +11,48 @@
  using namespace std;
 
 
-int main() {
+int main(int argc, char*argv[]) {
 	cout << "Start program" << endl;
 	
+	if(argv[1]){
+		int vflag_file = atoi(argv[1]);
 
-	cout << "Проверка: Ошибка двойного освобождения памяти. Должна быть ошибка" << endl;
-	mem_multiple_free();
+		if (vflag_file == 1 || vflag_file == 888){
+			cout << "Проверка: Ошибка двойного освобождения памяти. Должна быть ошибка" << endl;
+			mem_multiple_free();
+		}
 
-	cout << "Проверка: Ошибка с межпроцедурным вычислением индекса. Должна быть ошибка" << endl;
-	test_buffer_index();
+		if (vflag_file == 2 || vflag_file == 888){
+			cout << "Проверка: Ошибка с межпроцедурным вычислением индекса. Должна быть ошибка" << endl;
+			test_buffer_index();
+		}
 
-	cout << "Проверка: Ошибка разыменование нулевого указателя. Предупреждение о недостижимости кода" << endl;
-	test_null_pointer_dereference_warning();
+		if (vflag_file == 3 || vflag_file == 888){
+			cout << "Проверка: Ошибка разыменование нулевого указателя. Предупреждение о недостижимости кода" << endl;
+			test_null_pointer_dereference_warning();
+		}
 
-	cout << "Проверка: Ошибка разыменование нулевого указателя. Должна быть ошибка" << endl;
-	test_null_pointer_dereference();
+		if (vflag_file == 4 || vflag_file == 888){
+			cout << "Проверка: Ошибка разыменование нулевого указателя. Должна быть ошибка" << endl;
+			test_null_pointer_dereference();
+		}
 
-	cout << "Проверка: Ошибка обращения через указатель к удаленной переменной. Должна быть ошибка" << endl;
-	test_pointer_to_null();
+		if (vflag_file == 5 || vflag_file == 888){
+			cout << "Проверка: Ошибка обращения через указатель к удаленной переменной. Должна быть ошибка" << endl;
+			test_pointer_to_null();
+		}
 
-	cout << "Проверка: На чувствительность к путям выполнения. Код без ошибок" << endl;
-	test_ps_no_errors1();
+		if (vflag_file == 6 || vflag_file == 888){
+			cout << "Проверка: На чувствительность к путям выполнения. Код без ошибок" << endl;
+			test_ps_no_errors1();
+		}
 
-	cout << "Проверка: На чувствительность к путям выполнения. Код без ошибок" << endl;
-	test_ps_no_errors2();
-
+		if (vflag_file == 7 || vflag_file == 888){
+			cout << "Проверка: На чувствительность к путям выполнения. Код без ошибок" << endl;
+			test_ps_no_errors2();
+		}
+	}
+	
 	cout << "End program" << endl;
 	return 0;
 }
